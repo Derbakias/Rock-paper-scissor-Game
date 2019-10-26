fb19c2ca9ee2ca19d8c45b1eefbcd921181e9007
Derbakias <dimitrios.danousis@gmail.com>
2019-10-26 16:56:41 +0300

    complete version 0.1 (added about page)

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 var scoreP1 = document.querySelector(".P1 .score p");
 var scoreP2 = document.querySelector(".P2 .score p");
 var P1 = document.querySelector(".P1 .player-name p");
-var P2 = document.querySelector(".P2P2 .player-name p");
+var P2 = document.querySelector(".P2 .player-name p");
+var message = document.querySelector(".message p");
 var scissor = document.getElementById("scissor");
 var paper = document.getElementById("paper");
 var rock = document.getElementById("rock");
+var resetBtn = document.querySelector("#resetBtn");
 var player1Choice = "";
 var player2Choice = "";
 var playerChoice = [player1Choice, player2Choice];
 var turn = 0;
 var score = [0, 0]
 var choiceList = [scissor, paper, rock];
+P1.classList.add("active")
 
-
-// when player click add the choice to a list
+// when player click add the choice to the variable
 for(j = 0;j < choiceList.length; j++){
     choiceList[j].addEventListener("click", function(){
         playerChoice = this.id;
@@ -22,10 +24,16 @@ for(j = 0;j < choiceList.length; j++){
         if (turn === 1) {
             player2Choice = "";
             player1Choice = playerChoice;
+            P1.classList.remove("active")
+            P2.classList.add("active")
+            message.textContent = "New Game";
         }
         else if (turn === 2) {
             player2Choice = playerChoice;
+            P2.classList.remove("active")
+            P1.classList.add("active")
             turn = 0;
+            message.textContent = (" P1: " + player1Choice + " " + "P2: " + player2Choice);
             console.log(player1Choice, player2Choice);
         }
         checkWinner();
@@ -59,3 +67,17 @@ function checkWinner(){
     scoreP2.textContent = score[1];
 }
 
+//reset button event listener
+resetBtn.addEventListener("click", reset);
+
+//reset the message and the score
+function reset(){
+    message.textContent = "New Game";
+    score = [0, 0];
+    scoreP1.textContent = score[0];
+    scoreP2.textContent = score[1];
+    player1Choice = "";
+    player2Choice = "";
+    P1.classList.add("active");
+    P2.classList.remove("active");
+};
